027d177d7bdf443e7002fc473d36ba12ecd86c9a
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2025-02-14 21:28:00 +0500

    Adding the New Projects

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -24,6 +24,46 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-6xl">
         {/* Project 1 */}
+
+        <Link
+  to="https://tro-jobs.vercel.app/"
+  className={`shadow-lg rounded-lg p-6 flex flex-col justify-between transition-transform transform hover:scale-105 ${
+    theme
+      ? "bg-[--dark-card-bg] text-[--dark-text]"
+      : "bg-[--light-card-bg] text-[--light-text]"
+  }`}
+>
+<Link to="https://github.com/CODEMASTER-ABDULLAH-92/TroJobs" 
+  className="absolute right-4 top-4  text-white px-4 py-2 rounded-md bg-gray-700 transition"
+>
+  Source Code
+</Link>
+
+  <a
+    href="https://tro-jobs.vercel.app/"
+    className="text-2xl font-bold mb-4 hover:text-[#F53E32]"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Tro Jobs
+  </a>
+  <p className="mb-6 text-sm sm:text-base">
+    Tro Jobs is a MERN stack-powered job portal that connects employers and job
+    seekers. It features user authentication, job listings, and an intuitive
+    dashboard for seamless job applications.
+  </p>
+  <div className="flex flex-wrap gap-2">
+    {["React.js", "Node.js", "Express.js", "MongoDB", "Tailwind CSS", "Git", "Figma"].map((tech) => (
+      <span
+        key={tech}
+        className="bg-[--dark-btn-bg] text-white px-4 py-1 rounded-full text-sm"
+      >
+        {tech}
+      </span>
+    ))}
+  </div>
+</Link>
+
         <Link
           to="https://forge-gaming-app.vercel.app/"
           className={`shadow-lg rounded-lg p-6 flex flex-col justify-between transition-transform transform hover:scale-105 ${
@@ -38,8 +78,13 @@ const Projects = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Forge Gaming App
+            Forge App
           </a>
+          <Link to="https://github.com/CODEMASTER-ABDULLAH-92/Forge-Gaming-App" 
+  className="absolute right-4 top-4  text-white px-4 py-2 rounded-md bg-gray-700 transition"
+>
+  Source Code
+</Link>
           <p className="mb-6 text-sm sm:text-base">
             Forge Gaming App is a MERN stack-powered platform for gamers to
             explore news, reviews, and resources. It offers personalized
@@ -57,7 +102,6 @@ const Projects = () => {
             ))}
           </div>
         </Link>
-
         {/* Additional Projects */}
         <Link
           to="https://food-trove-app-h7hl.vercel.app/"
@@ -75,6 +119,11 @@ const Projects = () => {
           >
             FoodTrove
           </a>
+          <Link to="https://github.com/CODEMASTER-ABDULLAH-92/Forge-Gaming-App" 
+  className="absolute right-4 top-4  text-white px-4 py-2 rounded-md bg-gray-700 transition"
+>
+  Source Code
+</Link>
           <p className="mb-6 text-sm sm:text-base">
             FoodTrove is a MERN stack app showcasing web development skills.
             Perfect for food-related services, it integrates modern design and
@@ -108,6 +157,11 @@ const Projects = () => {
           >
             GYM24
           </a>
+          <Link to="https://github.com/CODEMASTER-ABDULLAH-92/CompleteGYM" 
+  className="absolute right-4 top-4  text-white px-4 py-2 rounded-md bg-gray-700 transition"
+>
+  Source Code
+</Link>
           <p className="mb-6 text-sm sm:text-base">
             GYM24 is a fitness-focused React app showcasing gym services,
             membership plans, and workout routines with a seamless and modern
@@ -141,6 +195,11 @@ const Projects = () => {
           >
             Electra Car
           </a>
+          <Link to="https://github.com/CODEMASTER-ABDULLAH-92/Car-App" 
+  className="absolute right-4 top-4  text-white px-4 py-2 rounded-md bg-gray-700 transition"
+>
+  Source Code
+</Link>
           <p className="mb-6 text-sm sm:text-base">
             A platform showcasing the latest in electric car technology,
             including detailed features, pricing, and sustainability benefits.
